af3604b6ae4b680b9081431c7f0e367d26e7c88d
Gins <kanallastyle@gmail.com>
2024-10-30 19:21:03 +0100

    Hola

--- ejercicio13.py
+++ ejercicio13.py
@@ -1,5 +1,28 @@
-"""Diseñar un algoritmo, que dados dos números, determine si su producto es positivo,
-nulo o negativo, sin realizar la multiplicación."""
-
-
-
+diff.astextplain.textconv=astextplain
+filter.lfs.clean=git-lfs clean -- %f
+filter.lfs.smudge=git-lfs smudge -- %f
+filter.lfs.process=git-lfs filter-process
+filter.lfs.required=true
+http.sslbackend=openssl
+http.sslcainfo=C:/Program Files/Git/mingw64/etc/ssl/certs/ca-bundle.crt
+core.autocrlf=true
+core.fscache=true
+core.symlinks=false
+pull.rebase=false
+credential.helper=manager
+credential.https://dev.azure.com.usehttppath=true
+init.defaultbranch=master
+user.name=Gins
+user.email=kanallastyle@gmail.com
+core.repositoryformatversion=0
+core.filemode=false
+core.bare=false
+core.logallrefupdates=true
+core.symlinks=false
+core.ignorecase=true
+remote.origin.url=https://github.com/MauroGins/programacion.git
+remote.origin.fetch=+refs/heads/*:refs/remotes/origin/*
+branch.main.remote=origin
+branch.main.merge=refs/heads/main
+branch.main.vscode-merge-base=origin/main
+branch.main.gk-merge-base=origin/main
